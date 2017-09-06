efe82989cc3a4c22cc0d0b48527552caf1e8323b
trancefam <trancefamva@gmail.com>
2017-09-05 23:44:24 -0400

    intial

---
+++ Assignment 1 - Make Change/Assignment 1 - Make Change.cpp
@@ -0,0 +1,24 @@
+// Assignment 1 - Make Change.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	double cost;
+	double amountPaid;
+
+
+	cout << "Enter the cost of the item to be purchased: " << endl;
+	cin >> cost;
+	cout << "Enter the amount paid: " << endl;
+	cin >> amountPaid;
+
+	if (amountPaid)
+
+    return 0;
+}
+
